33a257231b6f1a433158754ac4006082c507eacb
Varabiou Valiantsin <vxdator@gmail.com>
2019-09-29 22:55:40 +0300

    feat: ask for CI for Morse Decoder ed3ca775 :)

--- src/index.js
+++ src/index.js
@@ -38,6 +38,70 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+
+    let decodedString = '';
+
+    function MakeConvertTable( HumanMorse ){
+        const dotSign = {
+            source: '10',
+            morse: '.',
+        } ;
+        const dashSign = {
+            source: '11',
+            morse: '-',
+        } ;
+        const spaceSign = {
+            source: '**********',
+            morse: ' ',
+        }
+        let hashResTable = new Map()
+        for( let key in HumanMorse ){
+            let aLetter = HumanMorse[ key ] ;
+            let aSource = '';
+            for( let i=0; i<key.length; i++ ){
+
+                if( key[i] === dotSign[ 'morse' ] ){
+                    aSource = aSource + dotSign[ 'source' ]
+                }
+
+                if( key[i] === dashSign[ 'morse' ] ){
+                    aSource = aSource + dashSign[ 'source' ]
+                }
+
+            }
+
+            aSource = aSource.padStart( 10, '0' );
+            hashResTable.set( aSource.toString(), aLetter  );
+
+        }
+
+        hashResTable.set( spaceSign[ 'source' ] , spaceSign[ 'morse' ] );
+
+        return hashResTable;
+    }
+
+    function SplitSourceExpressionString( strExpression ){
+        const literalLength = 10;
+        let currStartIndex = 0; currEndIndex = currStartIndex + literalLength;
+        let retArray = [];
+        while( currStartIndex < strExpression.length ){
+            let currLiteral = strExpression.slice( currStartIndex, currStartIndex + literalLength );
+            currStartIndex += literalLength;
+            retArray = [ ...retArray, currLiteral ];
+        }
+
+        return retArray;
+    }
+
+    let suitableMorseTable = MakeConvertTable( MORSE_TABLE );
+    let slicedExpression = SplitSourceExpressionString( expr );
+
+    for( let sliceIndex = 0; sliceIndex < slicedExpression.length; sliceIndex++ ){
+        let currLiteral = slicedExpression[ sliceIndex ];
+        decodedString = decodedString + suitableMorseTable.get( currLiteral );
+    }
+
+    return decodedString;
     // write your solution here
 }
 
